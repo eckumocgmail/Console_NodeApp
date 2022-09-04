987e903db6df894bcf2b4e7877f0bfe686a1d938
Константин <eckumoc@gmail.com>
2022-09-04 06:17:44 +0300

    ╨б╨╛╨╖╨┤╨░╤О ╨╜╨╛╨▓╤Л╨╣ ╨┐╤А╨╛╨╡╨║╤В

---
+++ NodeModule/NodeProgram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Console_Node
+{
+    public class NodeProgram
+    {
+
+        /// <summary>
+        /// Исполнение команды
+        /// </summary>                
+        private static string Execute(string command)
+        {
+            ProcessStartInfo info = new ProcessStartInfo("CMD.exe", "/C " + command);
+
+            info.RedirectStandardError = true;
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+            System.Diagnostics.Process process = System.Diagnostics.Process.Start(info);
+
+            string response = process.StandardOutput.ReadToEnd();
+            return response;
+        }
+
+
+        public static string Run(string javascript)
+        {
+            System.IO.File.WriteAllText("index.js", javascript);
+            return Execute("node index.js");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                Run(@"
+                    var s='';
+                    for(let i=0; i<1024; i++){
+                        s+=String.fromCharCode(i);
+                    }
+                    console.info(s);
+                ")
+            );
+        }
+    }
+}
---
+++ bin/Debug/net5.0/index.js
@@ -0,0 +1,7 @@
+
+                var s='';
+                for(let i=0; i<1024; i++){
+                    s+=String.fromCharCode(i);
+                }
+                console.info(s);
+            
